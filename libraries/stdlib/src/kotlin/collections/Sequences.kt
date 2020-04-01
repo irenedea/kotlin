bcc80498cdf9201cc2e719e12aa8031c55e0bda4
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2020-04-01 22:43:10 +0300

    Use ArrayList instead of ArrayDeque for buffering items for shuffled

--- libraries/stdlib/src/kotlin/collections/Sequences.kt
+++ libraries/stdlib/src/kotlin/collections/Sequences.kt
@@ -115,14 +115,13 @@ public fun <T, R> Sequence<Pair<T, R>>.unzip(): Pair<List<T>, List<R>> {
 @SinceKotlin("1.4")
 public fun <T> Sequence<T>.shuffled(): Sequence<T> = shuffled(Random)
 
-@OptIn(ExperimentalStdlibApi::class)
 @SinceKotlin("1.4")
 public fun <T> Sequence<T>.shuffled(random: Random): Sequence<T> = sequence<T> {
-    val buffer = toCollection(ArrayDeque())
+    val buffer = toMutableList()
     while (buffer.isNotEmpty()) {
         val j = random.nextInt(buffer.size)
-        val first = buffer.removeFirst()
-        val value = if (j > 0) buffer.set(j - 1, first) else first
+        val last = @OptIn(ExperimentalStdlibApi::class) buffer.removeLast()
+        val value = if (j < buffer.size) buffer.set(j, last) else last
         yield(value)
     }
 }
